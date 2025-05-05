84f431184eefbb8b683a14cc17cf59618a786e98
Sarvesh Pandey <155083256+sarv-aspire01@users.noreply.github.com>
2025-05-05 21:43:04 +0530

    solution cpp file for add  problem cf1351A on 05/05/2025

---
+++ addcf1351a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <string.h>
+
+void fun()
+{
+
+     int a,b;
+    cin>>a>>b;
+   
+    cout<<(a+b)<<'\n';
+}
+int main()
+{
+    int t ;
+    cin>>t;
+    while (t--)
+    {
+        fun();
+    }
+    return 0;
+}
